529e5557e76831267d951cc3660ba174e509221a
Nick <nbw11@live.com>
2022-03-24 15:03:56 -0500

    CAC-103 C# Overview

---
+++ MyFirstApplication/House.cs
@@ -0,0 +1,14 @@
+﻿namespace MyFirstApplication;
+public class House
+{
+    string foundation;
+    string roofType;
+    string doorColor;
+    int windowSize;
+
+    public void DoorOpenClose()
+    {
+        Console.WriteLine("My door is open.");
+    }
+}
+
--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MyFirstApplication;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+House myHouse = new House();
+myHouse.DoorOpenClose();
