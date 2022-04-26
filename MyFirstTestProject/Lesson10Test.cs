c930f405d87f84cc6e609e9a814609b8bb34c4bd
Nick <nbw11@live.com>
2022-04-26 14:29:10 -0500

    final commit

---
+++ MyFirstTestProject/Lesson10Test.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstApplication;
+
+namespace MyFirstTestProject
+{
+    [TestClass]
+    public class Lesson10Test
+    {
+        [TestMethod]
+        public void FisrtName_LastName_ZipCode_Verify()
+        {
+            // arrange
+            string firstname = "George";
+            string lastname = "Washington";
+            int zipcode = 64119;
+        }
+    }
+}
